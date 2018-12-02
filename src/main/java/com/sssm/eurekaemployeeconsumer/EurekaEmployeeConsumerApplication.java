f16783c7c85bfc4e41b257db063b143c204df733
mandarshidhore <mandar.learnstuff@gmail.com>
2018-12-01 20:14:35 -0700

    consumer code

--- src/main/java/com/sssm/eurekaemployeeconsumer/EurekaEmployeeConsumerApplication.java
+++ src/main/java/com/sssm/eurekaemployeeconsumer/EurekaEmployeeConsumerApplication.java
@@ -2,11 +2,13 @@ package com.sssm.eurekaemployeeconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import com.sssm.eurekaemployeeconsumer.client.EmployeeConsumerClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaEmployeeConsumerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sssm/eurekaemployeeconsumer/client/EmployeeConsumerClient.java
+++ src/main/java/com/sssm/eurekaemployeeconsumer/client/EmployeeConsumerClient.java
@@ -1,22 +1,37 @@
 package com.sssm.eurekaemployeeconsumer.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
 @Component
 public class EmployeeConsumerClient {
 
+	@Autowired
+	private EurekaClient client;
+
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	// note that it takes some time for producer register correctly with the server
+	// if this method is invoked immediately after producer starts, it may report issues w/ DiscoveryClient
 	public void getEmployee() {
-		String baseURL = "http://localhost:8080/employee";
-		RestTemplate restTemplate = new RestTemplate();
+		System.out.println("==> Invoking employee-producer to get Employee information ====");
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = client.getNextServerFromEureka("employee-producer", false);
+		String serviceBaseUrl = instanceInfo.getHomePageUrl();
+
 		ResponseEntity<String> response = null;
 		try {
-			response = restTemplate.exchange(baseURL, HttpMethod.GET, getHeaders(), String.class);
+			response = restTemplate.getForEntity(serviceBaseUrl + "/employee", String.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
